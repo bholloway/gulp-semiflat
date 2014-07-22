7053d4ebc19430fa79c17bacb4c0d39f360e1d7b
benholloway <ben.holloway@3plearning.com>
2014-07-22 15:22:16 +1000

    used path to make glob more robust

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var through = require('through2');
 var minimatch = require('minimatch');
+var path = require('path');
 
 /**
  * Amend the base path of gulp vinyl files based on a <code>glob</code> to produce a somewhat flatter file tree at the
  * destination.
- * @param {string} [glob] A glob that will match the relative path of a file
+ * @param {string} [glob] A glob that will match the leading portion of a file <code>path</code>
  * @returns {Transform} A transform stream for gulp
  */
 module.exports = function(glob) {
   'use strict';
+  var source  = minimatch.makeRe(path.resolve(glob)).source.replace('$', '');
+  var pattern = new RegExp(source);
   return through.obj(function(file, encoding, done) {
-    var source   = minimatch.makeRe(file.cwd + glob.replace(/^\.?\//, '')).source.replace('$', '');
-    var pattern  = new RegExp(source);
     var analysis = pattern.exec(file.path);
     if (analysis) {
       file.base = analysis[0];

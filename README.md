8e2fa83f7b4726417eab454cad5eb020c2fc5200
benholloway <ben.holloway@3plearning.com>
2014-07-22 15:03:35 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-gulp-semiflat
-=============
+# gulp
 
-Amend the base path of gulp vinyl files based on a Glob to produce a somewhat flatter file tree at the destination.
+> Amend the base path of gulp vinyl files based on a <code>glob</code> to produce a somewhat flatter file tree at the
+destination.
+
+## Install
+
+Install with [npm](https://npmjs.org/package/gulp-semiflat).
+
+```
+npm install --save-dev gulp-semiflat
+```
+
+## Examples
+
+```js
+var gulp = require('gulp');
+var gulpSemiFlat = require('gulp-semiflat');
+
+gulp.task('default', function () {
+	return gulp.src('js/**/lib/**/*.js')  // base will be '/js'
+		.pipe(gulpSemiFlat('js/**/lib'))    // base is now '/js/../lib'
+		.pipe(gulp.dest('build/js');
+});
+```
---
+++ index.js
@@ -0,0 +1,22 @@
+var through = require('through2');
+var minimatch = require("minimatch");
+
+/**
+ * Amend the base path of gulp vinyl files based on a <code>glob</code> to produce a somewhat flatter file tree at the
+ * destination.
+ * @param {string} [glob] A glob that will match the relative path of a file
+ * @returns {Transform} A transform stream for gulp
+ */
+module.exports = function(glob) {
+  return through.obj(function(file, encoding, done) {
+    var source   = minimatch.makeRe(file.cwd + glob.replace(/^\.?\//, '')).source.replace('$', '');
+    var pattern  = new RegExp(source);
+    var analysis = pattern.exec(file.path);
+console.log(source);
+    if (analysis) {
+      file.base = analysis[0];
+    }
+    this.push(file);
+    done();
+  });
+};
